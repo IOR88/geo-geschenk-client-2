16c4f3658899eda3ca746db028a031b2c6cc7ec7
ior88 <t-32@o2.pl>
2016-04-13 23:48:58 +0200

    websockets

--- app/app.js
+++ app/app.js
@@ -1,35 +1,44 @@
 var express = require('express');
 var request = require('request');
+var body_parser = require('body-parser');
 
 var app = express();
 
 var SERVER_HOST = 'http://localhost:8000';
+var APP_PORT = 8001;
 
 app.set('view engine', 'ejs');
 app.set('views', 'app/views');
 
 app.use('/static', express.static(__dirname + '/static'));
 
+app.use( body_parser.json() );       // to support JSON-encoded bodies
+app.use(body_parser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
 app.get('/', function (req, res) {
   res.render('base');
 });
 
-app.post('/upload', function (req, res) {
-   /** API: localhost:8000/upload method:POST -> uploading files **/
-   var formData = {};
-   request.post({
-       url: SERVER_HOST + '/upload',
-       formData: formData
-   }, function(error, response){
-      if(error){
-          console.log(error);
-      }
-      console.log(response)
+app.post('/search', function (req, res, next) {
+    return request.post({
+        url: SERVER_HOST + '/search',
+        json: req.body
+    }, function(error, response){
 
-   })
+        if(error){
+          res.status(401).end();
+
+        } else {
+          res.status(200);
+          res.json(response.body);
+          res.end()
+        }
+    })
 });
 
 
-app.listen(8001, function () {
-  console.log('Example app listening on port 8001!');
+app.listen(APP_PORT, function () {
+  console.log('App listening on port ',APP_PORT);
 });
--- app/static/js/main.js
+++ app/static/js/main.js
@@ -2,36 +2,50 @@
 angular
     .module('geoGeschenk', ['ngFileUpload'])
 
-    .controller('gesController', ['Upload', function(Upload){
+    .controller('gesController',
+    ['Upload',
+    function(
+     Upload
+     ){
 
 
      var socket = io.connect('http://localhost:8000');
 
      socket.on('connect', function() {
+        /** when connection between sockets is established**/
         console.log('connected');
      });
 
      socket.on('geosquizzy', function(data){
+         /** gets information when geosquizzy algorithm is
+          *  traversing document**/
          console.log('geosquizzy', data);
      });
 
+     socket.on('data', function(data){
+         /** after emitting search event on
+          *  data event data will be send or
+          *  errors **/
+         console.log('data', data);
+     });
+
+     socket.on('demo', function(data){
+         /** can receive some errors during fetching demo
+          *  search query**/
+         console.log('demo errors', data)
+     });
+
      setTimeout(function(){
-         console.log('starting uploading dump file.');
-         uploadFile();
+         console.log('starting fetching demo data.');
+         socket.emit('demo')
 
-     }, 3000);
+     }, 1000);
 
-     function uploadFile(file){
-         /** TODO will be not handled by under node layer
-          *  TODO websocket should be moved to be handled by
-          *  TODO client -> server socket approach**/
+     setTimeout(function(){
+         console.log('starting geosquizzy algorithm execution.');
+         socket.emit('search', { query: {} });
+     }, 6000);
 
-         Upload.upload({
-                url: 'upload',
-                data: {file: file}
-            })
-
-     }
 
 
     }]);
